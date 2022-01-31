90723aecfc7b646a883f225fecf8a6979dc9d639
Julien ALEXANDRE <62375849+jualito@users.noreply.github.com>
2022-01-31 10:44:45 +0100

    Logique de récupération de l'utilisateur

--- internal/provider/data_source_user.go
+++ internal/provider/data_source_user.go
@@ -1,11 +1,17 @@
 package provider
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mattermost/mattermost-server/v6/model"
 )
 
 func dataSourceUser() *schema.Resource {
 	return &schema.Resource{
+		ReadContext: dataSourceUserRead,
+
 		Schema: map[string]*schema.Schema{
 			"username": {
 				Type:     schema.TypeString,
@@ -25,4 +31,21 @@ func dataSourceUser() *schema.Resource {
 			},
 		},
 	}
+}
+
+func dataSourceUserRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*model.Client4)
+	name := d.Get("username").(string)
+
+	user, _, err := c.GetUserByUsername(name, "")
+	if err != nil {
+		return diag.Errorf("cannot get user by name: %v", err)
+	}
+
+	d.SetId(user.Id)
+	d.Set("email", user.Email)
+	d.Set("first_name", user.FirstName)
+	d.Set("last_name", user.LastName)
+
+	return nil
 }
